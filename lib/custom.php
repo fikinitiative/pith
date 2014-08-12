cbb0657d633035710372230ffa45f1af97e741d3
rosday <sanudo@rodrigosanudo.com>
2014-08-12 12:30:10 +0200

    Add custom funtions & support for svg

---
+++ lib/custom.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Custom functions
+ */
+
+require_once locate_template('/lib/custom/menus.php');
+require_once locate_template('/lib/custom/others.php');
---
+++ lib/custom/others.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+*   Add support for upload SVG
+*/
+
+function cc_mime_types( $mimes ){
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
