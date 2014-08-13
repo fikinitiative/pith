3f1de5129532e748ecab767b6f239c9e4ccf18d5
rosday <sanudo@rodrigosanudo.com>
2014-08-13 13:19:02 +0200

    Add post thumbnails custom size & add thumbnails to blog

--- templates/content.php
+++ templates/content.php
@@ -1,4 +1,11 @@
 <article <?php post_class(); ?>>
+ <?php if ( has_post_thumbnail() ) { ?>
+      <figure>
+          <a href="<?php the_permalink(); ?>">
+              <?php the_post_thumbnail( 'post-custom-thumbnail', array('class' => 'img-responsive') ); ?>
+          </a>
+      </figure>
+  <?php } ?>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
