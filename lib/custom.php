98158b03f60ff1e04433c3fd5c8f8575ae410c85
rosday <sanudo@rodrigosanudo.com>
2014-08-18 12:34:35 +0200

    Add debug helper

--- lib/custom.php
+++ lib/custom.php
@@ -5,3 +5,4 @@
 
 require_once locate_template('/lib/custom/menus.php');
 require_once locate_template('/lib/custom/others.php');
+require_once locate_template('/lib/custom/debug.php'); // Bug testing only. Not to be used on a production site
