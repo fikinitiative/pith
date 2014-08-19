8938302357fe812ca02f29fcd8abfd409fe479ae
minayaserrano <minayaserrano@gmail.com>
2014-08-19 14:06:46 +0200

    Add bootrap-clearfix to product lists

--- lib/custom.php
+++ lib/custom.php
@@ -6,4 +6,5 @@
 require_once locate_template('/lib/custom/widgets.php');
 require_once locate_template('/lib/custom/menus.php');
 require_once locate_template('/lib/custom/others.php');
+require_once locate_template('/lib/custom/bootstrap_clearfix.php');
 require_once locate_template('/lib/custom/debug.php'); // Bug testing only. Not to be used on a production site
