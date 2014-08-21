0f16fc7d495d2006c6cd180cde645c4ea199dac5
Rodrigo Sañudo de Aymerich <rosday@Rodrigos-MacBook-Pro.local>
2014-08-21 10:20:32 +0200

    Add templates: no sidebar & sidebar right

--- lib/config.php
+++ lib/config.php
@@ -54,14 +54,16 @@ function roots_display_sidebar() {
      */
     array(
       'is_404',
-      'is_front_page'
+      'is_front_page',
+      array('is_page', array('Cart')),
+      array('is_singular', array('fik_product'))
     ),
     /**
      * Page template checks (via is_page_template())
      * Any of these page templates that return true won't show the sidebar
      */
     array(
-      'template-custom.php'
+      'template-no-sidebar.php'
     )
   );
 
