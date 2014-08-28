96f23079710f80b78b6ebba56ed2ceb55f34a552
Rodrigo Sañudo de Aymerich <rosday@Rodrigos-MacBook-Pro.local>
2014-08-28 16:58:41 +0200

    Add fik share plugin

--- lib/custom/plugins.php
+++ lib/custom/plugins.php
@@ -7,8 +7,14 @@ add_action('after_setup_theme', 'my_load_plugins');
 function my_load_plugins() {
 
     // Check to see if your plugin has already been loaded.
-	if (!function_exists('my_plugin_function_name')) {
+	if (!function_exists('flexmap_show_map')) {
 		// load plugin if not already loaded
 		include_once(TEMPLATEPATH.'/assets/plugins/wp-flexible-map/flexible-map.php');
+	}
+
+     // Check to see if your plugin has already been loaded.
+	if (!function_exists('fikSocialWidget_load_widgets')) {
+		// load plugin if not already loaded
+		include_once(TEMPLATEPATH.'/assets/plugins/fik-social-share/fik-social-share.php');
 	}
 }
