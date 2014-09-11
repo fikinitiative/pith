89f1b641ef62d5620945b8ec5e441a109150d892
Rodrigo Sañudo de Aymerich <rosday@Rodrigos-MacBook-Pro.local>
2014-09-11 10:53:16 +0200

    Add featured image to pages and fix minor problem with logo on responsive

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,7 @@
+<?php if ( has_post_thumbnail() ) { ?>
+      <figure class="featured-image">
+        <?php the_post_thumbnail( 'post-custom-thumbnail', array('class' => 'img-responsive') ); ?>
+      </figure>
+    <?php } ?>
 <?php the_content(); ?>
 <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
