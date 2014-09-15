af0a71c04b405701be641c1255193bc5ac961869
Bill Murphy <murphy@litio.net>
2014-09-15 13:11:59 +0200

    Function to load templates into variables

--- lib/custom/others.php
+++ lib/custom/others.php
@@ -15,3 +15,18 @@ add_filter( 'upload_mimes', 'cc_mime_types' );
 */
 
 add_filter('widget_text', 'do_shortcode');
+
+/* 
+ * This functions allows to get the contents of a template into a variable,
+ * needed for shortcodes. 
+ */
+
+if ( !function_exists( 'load_template_part' ) ){
+	function load_template_part($template_name, $part_name=null) {
+	    ob_start();
+	    get_template_part($template_name, $part_name);
+	    $var = ob_get_contents();
+	    ob_end_clean();
+	    return $var;
+	}
+}
