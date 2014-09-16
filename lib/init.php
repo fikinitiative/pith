338d995335a4eb0921220c7ba20d64be88766521
Rodrigo Sañudo de Aymerich <rosday@Rodrigos-MacBook-Pro.local>
2014-09-16 10:36:06 +0200

    Styling product carousel and change thumbnail

--- lib/init.php
+++ lib/init.php
@@ -24,7 +24,7 @@ function roots_setup() {
   add_image_size('post-custom-thumbnail', 1020, 600, true);
   add_image_size('latest-post-custom-thumbnail', 660, 300, true);
   add_image_size('store-product-custom-thumbnail', 728, 686, true);
-  add_image_size('store-single-product-custom-thumbnail', 1110, 1110, true);
+  add_image_size('store-single-product-custom-thumbnail', 1110, false);
 
   // Add post formats
   // http://codex.wordpress.org/Post_Formats
