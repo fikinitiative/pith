def5bf69bc62d5db790406cafcc1efe00bcd4dcf
Rodrigo Sañudo de Aymerich <rosday@Rodrigos-MacBook-Pro.local>
2014-09-18 12:39:36 +0200

    Add fikstores_badge function

--- templates/footer.php
+++ templates/footer.php
@@ -20,8 +20,8 @@
 <footer class="jumbotron closing">
     <div class="container">
         <div class="row">
-            <div class="col-sm-12 col-md-6"><p class="copy">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> | <a class="fik-stores-badge" href="http://fikstores.com/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/fik-logo-badge-white.svg'?>" /></a></p></div>
-            <?php if (has_nav_menu(footer_menu)) { ?>
+            <div class="col-sm-12 col-md-6"><p class="copy">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> | <?php the_fikstores_badge(); ?></p></div>
+            <?php if (has_nav_menu('footer_menu')) { ?>
             <div class="col-sm-12 col-md-6">
                <?php
                     wp_nav_menu(array(
