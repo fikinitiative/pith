09cbdc06039758dd681512332b5ecdb948060518
Rodrigo Sañudo de Aymerich <rosday@Rodrigos-MacBook-Pro.local>
2014-09-19 14:12:14 +0200

    Add comments icon & grunt

--- templates/comments.php
+++ templates/comments.php
@@ -6,7 +6,7 @@
 
 <?php if (have_comments()) : ?>
     <section id="comments">
-        <h3><?php printf(_n('One Response to &ldquo;%2$s&rdquo;', '%1$s Responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'roots'), number_format_i18n(get_comments_number()), get_the_title()); ?></h3>
+        <h3><span class="fa fa-comments-o comments-icon"></span><?php printf(_n('1 comment', '%1$s comments', get_comments_number(), 'roots'), number_format_i18n(get_comments_number()), get_the_title()); ?></h3>
 
         <ol class="media-list">
           <?php wp_list_comments(array('walker' => new Roots_Walker_Comment)); ?>
